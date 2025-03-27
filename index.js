90f46a227c7222b29ba6deebc31dc6ba130f73c9
dinodin <dinodin@dinodins-MacBook-Pro.local>
2025-03-27 01:03:00 +0000

    update regex

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// This will contain the JS for index.html - be sure to link to the correct file.
+const nameValue = document.getElementById("first_name");
+const lastName = document.getElementById("first_name");
+const nameValue = document.getElementById("first_name");
+const nameValue = document.getElementById("first_name");
+const nameValue = document.getElementById("first_name");
+
+nameValue.addEventListener("blur", (event) => {
+  validateName(event.target.value);
+});
+
+function validateName(first_name) {
+  const regex = /[a-z]{2,40}/gi;
+  return regex.test(first_name);
+}
